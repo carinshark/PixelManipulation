031082dcb6b60c90a9d4d5401bd1f7dd5dab6f72
Calvin <149830991+carinshark@users.noreply.github.com>
2026-02-21 19:00:38 -0700

    converted to np array

--- imageOpening.py
+++ imageOpening.py
@@ -1,7 +1,29 @@
 from PIL import Image
+import numpy as np
 
-im = Image.open("images1/ship.png")
+im = Image.open("images1/boxes.png")
 
 print(im.format,im.size,im.mode)
 
-im.show()
+
+# pixelGrid = np.array(im.getdata())
+
+# print()
+
+# pixelGrid=pixelGrid.reshape((im.size[1],im.size[0],len(im.mode)))
+
+
+# print(pixelGrid)
+
+# # manipulate pixels here
+
+
+# # convert to image
+
+# pixelGrid = pixelGrid.reshape(im.size[0]*im.size[1],len(im.mode))
+
+# newIm = Image.fromarray(pixelGrid,mode="RGBA")
+
+# newIm.show()
+
+
---
+++ lamdatest.py
@@ -0,0 +1,10 @@
+
+def b(x):
+    return x+1
+
+
+
+
+x = lambda a:b(a)
+
+print(x([1,2,3,4]))
